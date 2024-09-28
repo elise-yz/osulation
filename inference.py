298015ad4ca9d7a63b1bbb0f0a6b1c452c7470f4
ElemelonWind <c.yang.2023@gmail.com>
2024-09-28 16:53:38 -0400

    rm for loop

--- inference.py
+++ inference.py
@@ -37,43 +37,42 @@ while True:
     output = handGesture.process(rgbConvertedFrame)
     hands = output.multi_hand_landmarks
 
-
     if hands:
-        for hand in hands:
-            drawingTools.draw_landmarks(frame, hand)
-            landmarks = hand.landmark
-            lmList = []
-            for id, landmark in enumerate(landmarks):
-                cx, cy = int(landmark.x * frameWidth), int(landmark.y * frameHeight)
-                lmList.append([id, cx, cy])
-                if id == 5:
-                    x = int(landmark.x*frameWidth)
-                    y = int(landmark.y*frameHeight)
-                    cv2.circle(img=frame, center=(x,y), radius=30, color=(0, 255, 255))
-                    mousePositionX = screenWidth/frameWidth*x
-                    mousePositionY = screenHeight/frameHeight*y
-                    pyautogui.moveTo(mousePositionX, mousePositionY)
-            indexX = 0
-            indexY = 0
-            indexMid = 0
-            handBottomX = 0
-            handBottomY = 0
-            pinkyX = 0
-            pinkyY = 0
-            for lms in lmList:
-                if lms[0] == 7:
-                    indexX, indexY = lms[1], lms[2]
-                elif lms[0] == 5:
-                    indexMid = lms[2]
-                elif lms[0] == 19:
-                    pinkyX, pinkyY = lms[1], lms[2]
-                elif lms[0] == 0:
-                    handBottomX, handBottomY = lms[1], lms[2]
-            if (indexY < handBottomY) and (indexY > indexMid):
-                cv2.rectangle(rgbConvertedFrame, (indexX, indexY), (pinkyX, handBottomY), (0, 0, 255), 2)
-                close_fist()
-            else:
-                open_hand()
+        hand = hands[0]
+        drawingTools.draw_landmarks(frame, hand)
+        landmarks = hand.landmark
+        lmList = []
+        for id, landmark in enumerate(landmarks):
+            cx, cy = int(landmark.x * frameWidth), int(landmark.y * frameHeight)
+            lmList.append([id, cx, cy])
+            if id == 5:
+                x = int(landmark.x*frameWidth)
+                y = int(landmark.y*frameHeight)
+                cv2.circle(img=frame, center=(x,y), radius=30, color=(0, 255, 255))
+                mousePositionX = screenWidth/frameWidth*x
+                mousePositionY = screenHeight/frameHeight*y
+                pyautogui.moveTo(mousePositionX, mousePositionY)
+        indexX = 0
+        indexY = 0
+        indexMid = 0
+        handBottomX = 0
+        handBottomY = 0
+        pinkyX = 0
+        pinkyY = 0
+        for lms in lmList:
+            if lms[0] == 7:
+                indexX, indexY = lms[1], lms[2]
+            elif lms[0] == 5:
+                indexMid = lms[2]
+            elif lms[0] == 19:
+                pinkyX, pinkyY = lms[1], lms[2]
+            elif lms[0] == 0:
+                handBottomX, handBottomY = lms[1], lms[2]
+        if (indexY < handBottomY) and (indexY > indexMid):
+            cv2.rectangle(rgbConvertedFrame, (indexX, indexY), (pinkyX, handBottomY), (0, 0, 255), 2)
+            close_fist()
+        else:
+            open_hand()
 
     # cv2.imshow('Virtual Mouse', frame)
     # cv2.waitKey(1)
