9ad1dde15004242935a6ffc4bed18dc9cbc0ed25
ElemelonWind <c.yang.2023@gmail.com>
2024-09-28 14:13:54 -0400

    does not work

--- osulation.py
+++ osulation.py
@@ -1,33 +1,77 @@
+# Import the InferencePipeline object
+from inference import InferencePipeline
 import cv2
-from cv2_enumerate_cameras import enumerate_cameras
+from dotenv import load_dotenv
+import os
+import pyautogui
 
-CAMERA_NAME = "Logitech Webcam C925e" # Name of the webcame
-CAMERA_INDEX = 0
+def move_to(x, y):
+    pyautogui.moveTo(x, y)
+    # if click:
+    #     pyautogui.click() 
 
-for camera_info in enumerate_cameras():
-    if camera_info.name == CAMERA_NAME:
-        CAMERA_INDEX = camera_info.index
+def pause_game():
+    pyautogui.press('esc')
 
-stream = cv2.VideoCapture(CAMERA_INDEX) # Open video based on camera index
+def close_fist(): 
+    pyautogui.mouseDown()
 
-if not stream.isOpened(): # If stream is not working
-    print("No stream :(")
-    exit()
+def open_hand():
+    pyautogui.mouseUp()
 
-fps = stream.get(cv2.CAP_PROP_FPS) 
-width = int(stream.get(3)) # Set video dimensions
-height = int(stream.get(4))
+load_dotenv()
 
-while True:
-    ret, frame = stream.read()
-    if not ret: # If no frames are returned
-        print("No camera info")
-        break
+screen_width, screen_height = pyautogui.size()
+
+api_key = os.getenv("ROBOFLOW_API_KEY")
+
+def scale(x, y, screen_width, screen_height, img_width, img_height):
+    x = int(x * screen_width / img_width)
+    y = int(y * screen_height / img_height)
+    # reflect x
+    x = screen_width - x
+    return x, y
+
+def my_sink(result, video_frame):
+    # move mouse to center of the hand
+    # print(result["model_1"])
+    try:
+        x = int(result["model_1"][0]["predictions"][0]["x"])
+        y = int(result["model_1"][0]["predictions"][0]["y"])
+        image_width = result["model_1"][0]["image"]["width"]
+        image_height = result["model_1"][0]["image"]["height"]
+        newX, newY = scale(x, y, screen_width, screen_height, image_width, image_height)
+        move_to(newX, newY)
+    except:
+        pass
+
+    # click if fist, unclick if palm
+    try:
+        if result["model_1"][0]["predictions"][0]["class"] == "Rock":
+            close_fist()
+            # print("Fist")
+        else:
+            open_hand()
+            # print("Palm")
+    except:
+        pass
     
-    frame = cv2.resize(frame, (width, height))
-    cv2.imshow("Osulatin!", frame)
-    if cv2.waitKey(1) == ord('q'): # Press q to quit
-        break
 
-stream.release()
-cv2.destroyAllWindows() 
+# initialize a pipeline object
+pipeline = InferencePipeline.init_with_workflow(
+    api_key=api_key,
+    workspace_name="osulation",
+    workflow_id="custom-workflow-2",
+    video_reference=0, # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
+    max_fps=10,
+    on_prediction=my_sink
+)
+pipeline.start() #start the pipeline
+# pipeline.join() #wait for the pipeline thread to finish
+
+# stop pipeline after 30 seconds
+import time
+time.sleep(30)
+print("Terminating pipeline")
+
+pipeline.terminate() #stop the pipeline
